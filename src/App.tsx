f82b88fcb2044dd53022edd9915706a1307c081c
Jintu Das <jintudas047@gmail.com>
2024-10-11 13:07:22 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <main className="container mx-auto">
+      <h1 className="scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-5xl">
+        Resume Builder
+      </h1>
+      <p>
+        This is a simple resume builder application that allows you to create
+        and download your resume in PDF format.
+      </p>
+    </main>
+  );
+}
+
+export default App;
